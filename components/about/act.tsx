ea67ecdeec787c1f20698113e28344a92fbb279e
michael <mike124691@gmail.com>
2025-09-15 19:23:55 -0400

    update website..

--- components/about/act.tsx
+++ components/about/act.tsx
@@ -53,20 +53,20 @@ export default function Act() {
             <div className="flex flex-col ">
                 <div className=" ml-1">
                     <div className="font-semibold text-[15px] text-foreground">
-                        
+                        Natural Language Processing Research Lead
                     </div>
                     <div className="font-normal text-[15px] text-alt-foreground ">
-                        Georgia Tech Concert Orchestra
+                        University of Groningen
                     </div>
                 </div>
             </div>
 
             <div className="flex flex-col text-right">
                 <div className="font-semibold text-[15px] text-foreground">
-                    Sept. 2025 - Present
+                    June 2023 - Nov. 2023
                 </div>
                 <div className="font-normal text-[15px] text-alt-foreground ">
-                    First Violinist
+                    Remote
                 </div>
             </div>
         </div>
